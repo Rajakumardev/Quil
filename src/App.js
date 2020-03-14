c7e246c2fcead20f765ffdf15a92602fe6a20467
Rajakumardev <rajakumarofficial@gmail.com>
2020-03-14 19:15:23 +0530

    firebase sdk secure setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import * as firebase from 'firebase'
 
 function App() {
+  useEffect(()=> {
+    firebase.auth().onAuthStateChanged((currentUser) => {
+      if(!currentUser){
+        console.log('Not Authenticated')
+      }
+    })
+  })
   return (
     <div className="App">
         Quil
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as firebase from 'firebase';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+firebase.initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_FIREBASE_APPID
+})
