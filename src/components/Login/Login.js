32e691c36d7227c870e61de53d82d264819096fd
Rajakumardev <rajakumarofficial@gmail.com>
2020-04-12 13:48:03 +0530

    signup complete

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,25 +1,38 @@
-import React, { useEffect  } from 'react'
+import React, { useEffect,useState  } from 'react'
 import { useForm } from 'react-hook-form'
 import * as firebase from 'firebase'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 
 function Signup() {
     
     const { register, handleSubmit, errors } = useForm()
-
+    const history = useHistory()
+    const [loginMessage, setloginMessage] = useState('')
     useEffect(() => {
         firebase.auth().onAuthStateChanged((currentUser) => {
             if (!currentUser) {
                 console.log('Not Authenticated')
             } else {
-                //redirect to feed
-                console.log('redirect to feed')
+                history.push('/')
             }
         })
     })
     
 
     const loginUser = formData => {
+        firebase.auth().signInWithEmailAndPassword(formData.userEmail,formData.userPassword).catch(error => {
+            console.log(error.code)
+            switch(error.code){
+                case "auth/wrong-password":
+                    setloginMessage('username or password incorrect!')
+                    break;
+                case "auth/user-not-found":
+                    setloginMessage('username or password incorrect!')
+                    break;
+                default:
+                    setloginMessage('something went wrong!')
+            }
+        })
     }
 
     return (
@@ -40,7 +53,7 @@ function Signup() {
                 ref={register({ minLength: 8, required: true })}
                 className="border-b-2 p-2 w-full border-indigo-200" />
             <i className="text-red-600 mb-6 block px-2 h-2 text-xs">
-                {errors.userPassword && 'password is required!'}
+                {errors.userPassword && 'password should contain atleast 8 chars!'}
             </i>
             <input
                 type="submit"
@@ -50,6 +63,9 @@ function Signup() {
             <div className="mb-6 text-sm text-center">
                 Don't have an account, then <Link to="/signup" className="text-indigo-500 hover:underline">signup</Link>
             </div>
+            {loginMessage !== '' && <div className="mt-6 text-red-600 text-sm text-center">
+                {loginMessage}
+            </div>}
         </form>
     )
 }
